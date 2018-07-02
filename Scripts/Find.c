a0c67dadcd022d7e659d8ab5f505c3df6608f514
mgood7123 <smallville7123@gmail.com>
2018-07-03 04:08:40 +1000

    Update Find.c
    Added -o option - output all text to a file

--- Scripts/Find.c
+++ Scripts/Find.c
@@ -5,9 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define ps(x) { printf("%s = %s\n", #x, x); }
-#define pi(x) { printf("%s = %d\n", #x, x); }
-
 int depth;
 
 char * quote(char * str) {
@@ -34,22 +31,30 @@ typedef enum {
 str_to_argv_err_t string_to_argv(char const * str, int * argc_p, char *** argv_p);
 void builtin__argv(int argc, char ** argv);
 
-#define print_usage(x) printf("%s\n", x);
-#define print_usage_command(x) printf("\t%s\n", x);
-#define print_usage_command_info(x) printf("\t\t\t\t%s\n", x);
+FILE * file;
 
-void usage(void){
+#define print_usage(x) fprintf(file, "%s\n", x);
+#define print_usage_command(x) fprintf(file, "\t%s\n", x);
+#define print_usage_command_info(x) fprintf(file, "\t\t\t\t%s\n", x);
+
+void fusage(void){
 	print_usage("usage: find [path] [option]")
 	print_usage_command("-name [file]")
 	print_usage_command_info("searches [path] recursively for [file]")
+	print_usage_command("-o [file]")
+	print_usage_command_info("redirects all output into [file]")
 }
+
+int is_out = 0;
 
 void find(const char *name)
 {
+	if (!is_out) file = stdout;
 	char *is_name = NULL, *is_type = NULL;
 	int argc, is_root = 0, is_slash = 0;
 	char ** argv;
 	string_to_argv(name, &argc, &argv);
+	//builtin__argv(argc, argv);
 	// case 1, if path is root itself, correct d_name
 	if ((argv[0])[0] == '/' && strlen(argv[0]) == 1) is_root = 1;
 	// case 2, if path ends in / but is not root itself, remove it
@@ -68,12 +73,34 @@ void find(const char *name)
 			is_type = argv[i];
 		}
 		else if (!strcmp(argv[i], "-h") || !strcmp(argv[i],"--help")) {
-			usage();
+			if (is_out) {
+				fclose(file);
+				file = stdout;
+				is_out = 0;
+			}
+			fusage();
 			return;
 		}
+		else if (!strcmp(argv[i], "-o")) {
+			i++;
+			if (!is_out) {
+				is_out = 1;
+				file = fopen(argv[i], "a+");
+				if (!file) {
+					puts("failed to set out");
+					file = stdout;
+					is_out = 0;
+				}
+			}
+		}
 		else if ((argv[i])[0]== '-') {
-			printf("invalid command: '%s'\n", argv[i]);
-			usage();
+			if (is_out) {
+				fclose(file);
+				file = stdout;
+				is_out = 0;
+			}
+			fprintf(file, "invalid command: '%s'\n", argv[i]);
+			fusage();
 			return;
 		}	
 	}
@@ -81,17 +108,16 @@ void find(const char *name)
     struct dirent *entry;
 
     if (!(dir = opendir(argv[0]))) {
-    	printf("%s/ (unreadable)\n", argv[0]);
+    	fprintf(file, "%s/ (unreadable)\n", argv[0]);
         return;
     }
     
     char pathD[1024];
     snprintf(pathD, sizeof(pathD), "%s%s", argv[0], is_root?"":(is_slash?"/":"/"));
-    
     if (is_name) {
-		if (strcmp(is_name, argv[0]) == 0) printf("%s\n", pathD);
+		if (strcmp(is_name, argv[0]) == 0) fprintf(file, "%s\n", pathD);
 	}
-	else printf("%s\n", pathD);
+	else fprintf(file, "%s\n", pathD);
     
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR) {
@@ -110,12 +136,17 @@ void find(const char *name)
             char pathF[1024];
             snprintf(pathF, sizeof(pathF), "%s%s%s", argv[0], is_root?"":(is_slash?"/":"/"), entry->d_name);
 			if (is_name) {
-				if (strcmp(is_name, entry->d_name) == 0) printf("%s\n", pathF);
+				if (strcmp(is_name, entry->d_name) == 0) fprintf(file, "%s\n", pathF);
 			}
-			else printf("%s\n", pathF);
+			else fprintf(file, "%s\n", pathF);
         }
     }
     closedir(dir);
+    if (is_out && !depth) {
+		fclose(file);
+		file = stdout;
+		is_out = 0;
+	}
 }
 
 int main(int argc, char * argv[]) {
@@ -338,6 +369,6 @@ copy_cooked_string(char ** dest_p, char ** src_p)
 }
 
 void builtin__argv(int argc, char ** argv) {
-	printf("argc = %d\n", argc);
-	for (int i = 0; i<argc; i++) printf("argv[%d] = %s\n", i, argv[i]);
+	fprintf(file, "argc = %d\n", argc);
+	for (int i = 0; i<argc; i++) fprintf(file, "argv[%d] = %s\n", i, argv[i]);
 }
