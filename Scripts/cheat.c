99a9d987d05ebcd53d751665ec4a50fad80c03f2
Lee Jeong Seop <dztall@hanmail.net>
2018-07-11 18:12:23 +0900

    cheat.c

---
+++ Scripts/cheat.c
@@ -0,0 +1,21 @@
+﻿//Original source code found at : https://github.com/chubin/cheat.sh
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char lang[512] = "c";
+	printf("Programming language? ");
+	scanf("%s", lang);
+
+	char keywords[512] = "append+file";
+	printf("Keywords?\nex) append+file ");
+	scanf("%s", keywords);
+
+	char command[512] = "";
+	sprintf(command, "curl cht.sh/%s/%s", city, keywords);
+	system(command);
+
+	return 0;
+}
