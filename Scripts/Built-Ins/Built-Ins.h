4d74ccf942310990ff6d1505b1088d60e556d427
mgood7123 <smallville7123@gmail.com>
2018-08-02 08:36:47 +1000

    add compatibility for git, decode ~/ in whereis

--- Scripts/Built-Ins/Built-Ins.h
+++ Scripts/Built-Ins/Built-Ins.h
@@ -5,6 +5,14 @@
 	for(int i = 0;i<times;i++) { what }; \
 }
 
+char * print_size(int *print_size) { // superceeded by gits version
+	char *print_size_unit = "B";
+	if (*print_size > 1024) { *print_size /= 1024; print_size_unit = "K"; }
+	if (*print_size > 1024) { *print_size /= 1024; print_size_unit = "M"; }
+	if (*print_size > 1024) { *print_size /= 1024; print_size_unit = "G"; }
+	return print_size_unit;
+}
+
 #ifndef SHELL
 struct shell {
 	char * name;
@@ -42,30 +50,232 @@ shell.name = "Shell";
 #define CURRENT_FUNCTION DEBUG2 printf("->called %s() at line %d from %s\n", __func__, __LINE__, __FILE__);
 #endif
 
-#define pc(x) printf("%s = %c\n", #x, x);
-#define puc(x) printf("%s = %c\n", #x, x);
-#define ps(x) printf("%s = %s\n", #x, x);
-#define pus(x) printf("%s = %s\n", #x, x);
-#define pi(x) printf("%s = %d\n", #x, x);
-#define pui(x) printf("%s = %u\n", #x, x);
-#define pd(x) printf("%s = %f\n", #x, x);
-#define pud(x) printf("%s = %f\n", #x, x);
-#define pl(x) printf("%s = %l\n", #x, x);
-#define pul(x) printf("%s = %lu\n", #x, x);
-#define pll(x) printf("%s = %ll\n", #x, x);
-#define pull(x) printf("%s = %llu\n", #x, x);
-#define psz(x) printf("%s = %zu\n", #x, x);
-#define pp(x) printf("%s = %p\n", #x, x);
-#define px(x) printf("%s = %02x\n", #x, x);
-#define p(x) printf(#x);
+/*
+
+#if !defined(_INTTYPES_H_)
+#define _INTTYPES_H_
+
+#  define __PRI_8_LENGTH_MODIFIER__ "hh"
+#  define __PRI_64_LENGTH_MODIFIER__ "ll"
+#  define __SCN_64_LENGTH_MODIFIER__ "ll"
+#  define __PRI_MAX_LENGTH_MODIFIER__ "j"
+#  define __SCN_MAX_LENGTH_MODIFIER__ "j"
+
+#  define PRId8         __PRI_8_LENGTH_MODIFIER__ "d"
+#  define PRIi8         __PRI_8_LENGTH_MODIFIER__ "i"
+#  define PRIo8         __PRI_8_LENGTH_MODIFIER__ "o"
+#  define PRIu8         __PRI_8_LENGTH_MODIFIER__ "u"
+#  define PRIx8         __PRI_8_LENGTH_MODIFIER__ "x"
+#  define PRIX8         __PRI_8_LENGTH_MODIFIER__ "X"
+
+#  define PRId16        "hd"
+#  define PRIi16        "hi"
+#  define PRIo16        "ho"
+#  define PRIu16        "hu"
+#  define PRIx16        "hx"
+#  define PRIX16        "hX"
+
+#  define PRId32        "d"
+#  define PRIi32        "i"
+#  define PRIo32        "o"
+#  define PRIu32        "u"
+#  define PRIx32        "x"
+#  define PRIX32        "X"
+
+#  define PRId64        __PRI_64_LENGTH_MODIFIER__ "d"
+#  define PRIi64        __PRI_64_LENGTH_MODIFIER__ "i"
+#  define PRIo64        __PRI_64_LENGTH_MODIFIER__ "o"
+#  define PRIu64        __PRI_64_LENGTH_MODIFIER__ "u"
+#  define PRIx64        __PRI_64_LENGTH_MODIFIER__ "x"
+#  define PRIX64        __PRI_64_LENGTH_MODIFIER__ "X"
+
+#  define PRIdLEAST8    PRId8
+#  define PRIiLEAST8    PRIi8
+#  define PRIoLEAST8    PRIo8
+#  define PRIuLEAST8    PRIu8
+#  define PRIxLEAST8    PRIx8
+#  define PRIXLEAST8    PRIX8
+
+#  define PRIdLEAST16   PRId16
+#  define PRIiLEAST16   PRIi16
+#  define PRIoLEAST16   PRIo16
+#  define PRIuLEAST16   PRIu16
+#  define PRIxLEAST16   PRIx16
+#  define PRIXLEAST16   PRIX16
+
+#  define PRIdLEAST32   PRId32
+#  define PRIiLEAST32   PRIi32
+#  define PRIoLEAST32   PRIo32
+#  define PRIuLEAST32   PRIu32
+#  define PRIxLEAST32   PRIx32
+#  define PRIXLEAST32   PRIX32
+
+#  define PRIdLEAST64   PRId64
+#  define PRIiLEAST64   PRIi64
+#  define PRIoLEAST64   PRIo64
+#  define PRIuLEAST64   PRIu64
+#  define PRIxLEAST64   PRIx64
+#  define PRIXLEAST64   PRIX64
+
+#  define PRIdFAST8     PRId8
+#  define PRIiFAST8     PRIi8
+#  define PRIoFAST8     PRIo8
+#  define PRIuFAST8     PRIu8
+#  define PRIxFAST8     PRIx8
+#  define PRIXFAST8     PRIX8
+
+#  define PRIdFAST16    PRId16
+#  define PRIiFAST16    PRIi16
+#  define PRIoFAST16    PRIo16
+#  define PRIuFAST16    PRIu16
+#  define PRIxFAST16    PRIx16
+#  define PRIXFAST16    PRIX16
+
+#  define PRIdFAST32    PRId32
+#  define PRIiFAST32    PRIi32
+#  define PRIoFAST32    PRIo32
+#  define PRIuFAST32    PRIu32
+#  define PRIxFAST32    PRIx32
+#  define PRIXFAST32    PRIX32
+
+#  define PRIdFAST64    PRId64
+#  define PRIiFAST64    PRIi64
+#  define PRIoFAST64    PRIo64
+#  define PRIuFAST64    PRIu64
+#  define PRIxFAST64    PRIx64
+#  define PRIXFAST64    PRIX64
+
+// int32_t is 'int', but intptr_t is 'long'.
+#  define PRIdPTR       "ld"
+#  define PRIiPTR       "li"
+#  define PRIoPTR       "lo"
+#  define PRIuPTR       "lu"
+#  define PRIxPTR       "lx"
+#  define PRIXPTR       "lX"
+
+#  define PRIdMAX        __PRI_MAX_LENGTH_MODIFIER__ "d"
+#  define PRIiMAX        __PRI_MAX_LENGTH_MODIFIER__ "i"
+#  define PRIoMAX        __PRI_MAX_LENGTH_MODIFIER__ "o"
+#  define PRIuMAX        __PRI_MAX_LENGTH_MODIFIER__ "u"
+#  define PRIxMAX        __PRI_MAX_LENGTH_MODIFIER__ "x"
+#  define PRIXMAX        __PRI_MAX_LENGTH_MODIFIER__ "X"
+
+#  define SCNd8         __PRI_8_LENGTH_MODIFIER__ "d"
+#  define SCNi8         __PRI_8_LENGTH_MODIFIER__ "i"
+#  define SCNo8         __PRI_8_LENGTH_MODIFIER__ "o"
+#  define SCNu8         __PRI_8_LENGTH_MODIFIER__ "u"
+#  define SCNx8         __PRI_8_LENGTH_MODIFIER__ "x"
+
+#  define SCNd16        "hd"
+#  define SCNi16        "hi"
+#  define SCNo16        "ho"
+#  define SCNu16        "hu"
+#  define SCNx16        "hx"
+
+#  define SCNd32        "d"
+#  define SCNi32        "i"
+#  define SCNo32        "o"
+#  define SCNu32        "u"
+#  define SCNx32        "x"
+
+#  define SCNd64        __SCN_64_LENGTH_MODIFIER__ "d"
+#  define SCNi64        __SCN_64_LENGTH_MODIFIER__ "i"
+#  define SCNo64        __SCN_64_LENGTH_MODIFIER__ "o"
+#  define SCNu64        __SCN_64_LENGTH_MODIFIER__ "u"
+#  define SCNx64        __SCN_64_LENGTH_MODIFIER__ "x"
+
+#  define SCNdLEAST8    SCNd8
+#  define SCNiLEAST8    SCNi8
+#  define SCNoLEAST8    SCNo8
+#  define SCNuLEAST8    SCNu8
+#  define SCNxLEAST8    SCNx8
+
+#  define SCNdLEAST16   SCNd16
+#  define SCNiLEAST16   SCNi16
+#  define SCNoLEAST16   SCNo16
+#  define SCNuLEAST16   SCNu16
+#  define SCNxLEAST16   SCNx16
+
+#  define SCNdLEAST32   SCNd32
+#  define SCNiLEAST32   SCNi32
+#  define SCNoLEAST32   SCNo32
+#  define SCNuLEAST32   SCNu32
+#  define SCNxLEAST32   SCNx32
+
+#  define SCNdLEAST64   SCNd64
+#  define SCNiLEAST64   SCNi64
+#  define SCNoLEAST64   SCNo64
+#  define SCNuLEAST64   SCNu64
+#  define SCNxLEAST64   SCNx64
+
+#  define SCNdFAST8     SCNd8
+#  define SCNiFAST8     SCNi8
+#  define SCNoFAST8     SCNo8
+#  define SCNuFAST8     SCNu8
+#  define SCNxFAST8     SCNx8
+
+#  define SCNdFAST16    SCNd16
+#  define SCNiFAST16    SCNi16
+#  define SCNoFAST16    SCNo16
+#  define SCNuFAST16    SCNu16
+#  define SCNxFAST16    SCNx16
+
+#  define SCNdFAST32    SCNd32
+#  define SCNiFAST32    SCNi32
+#  define SCNoFAST32    SCNo32
+#  define SCNuFAST32    SCNu32
+#  define SCNxFAST32    SCNx32
+
+#  define SCNdFAST64    SCNd64
+#  define SCNiFAST64    SCNi64
+#  define SCNoFAST64    SCNo64
+#  define SCNuFAST64    SCNu64
+#  define SCNxFAST64    SCNx64
+
+#  define SCNdPTR       "ld"
+#  define SCNiPTR       "li"
+#  define SCNoPTR       "lo"
+#  define SCNuPTR       "lu"
+#  define SCNxPTR       "lx"
+
+#  define SCNdMAX       __SCN_MAX_LENGTH_MODIFIER__ "d"
+#  define SCNiMAX       __SCN_MAX_LENGTH_MODIFIER__ "i"
+#  define SCNoMAX       __SCN_MAX_LENGTH_MODIFIER__ "o"
+#  define SCNuMAX       __SCN_MAX_LENGTH_MODIFIER__ "u"
+#  define SCNxMAX       __SCN_MAX_LENGTH_MODIFIER__ "x"
+*/
+
+#include <inttypes.h>
+
+#define puint64(x) printf("%s = %" PRIu64 "\n", #x, x);
+#define puint32(x) printf("%s = %" PRIu32 "\n", #x, x);
+#define puint16(x) printf("%s = %" PRIu16 "\n", #x, x);
+#define puint8(x)  printf("%s = %" PRIu8 "\n", #x, x);
+
+#define pc(x)      printf("%s = %c\n", #x, x);
+#define puc(x)     printf("%s = %c\n", #x, x);
+#define ps(x)      printf("%s = %s\n", #x, x);
+#define pus(x)     printf("%s = %s\n", #x, x);
+#define pi(x)      printf("%s = %d\n", #x, x);
+#define pui(x)     printf("%s = %u\n", #x, x);
+#define pd(x)      printf("%s = %f\n", #x, x);
+#define pud(x)     printf("%s = %f\n", #x, x);
+#define pl(x)      printf("%s = %l\n", #x, x);
+#define pul(x)     printf("%s = %lu\n", #x, x);
+#define pll(x)     printf("%s = %ll\n", #x, x);
+#define pull(x)    printf("%s = %llu\n", #x, x);
+#define psize_t(x) printf("%s = %zu\n", #x, x);
+#define pp(x)      printf("%s = %p\n", #x, x);
+#define px(x)      printf("%s = %02x\n", #x, x);
+#define p(x)       printf(#x);
 
 #include "libstring.h"
 #include "env.h"
 #include <SDL.h>
 #include <inttypes.h>
+#include "regex.h"
 #include "argv.h"
 #include "grep.h" // not needed but included if it ever is
-#include "regex.h"
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -84,6 +294,26 @@ shell.name = "Shell";
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
+
+int getline_stdin(char ** input) {
+	int total;
+	char * t = malloc(4096);
+	char charcurrent = 0;
+	while(charcurrent!='\n') {
+		charcurrent = getchar();
+		if (charcurrent != '\n') total = sprintf(t, "%s%c", t, charcurrent);
+	}
+	if (!total) {
+		free(t);
+		return 0;
+	}
+	total++;
+	*input = malloc(total);
+	memset(*input, 0, total);
+	strncpy(*input, t, total-1);
+	free(t);
+	return total-1;
+}
 
 
 char * quote (char * str) { 
@@ -612,34 +842,34 @@ char * builtin__whereis(char ** f, const char * extention, int skip_arg0) {
 		char *hardcoded_platform_specific_path_separator = ":";
 		char * pathtmp = strdup(path);
 		for (char *tok = strtok(pathtmp, hardcoded_platform_specific_path_separator); tok; tok = strtok(NULL, hardcoded_platform_specific_path_separator)) {
-			sprintf(ph, "%s/%s%s", tok, file, extention);
-			printf("tryingA %s....   \n", ph);
+			sprintf(ph, "/var/%s/%s%s", tok+4, file, extention);
+			printf("trying %s\n", ph);
 			if (access(ph, F_OK) == 0) {
-	      		printf("foundA at %s\n", ph);
+	      		printf("found '%s' at '%s'\n", file, ph);
 	      		return ph;
 	      	}
 		}
 		free(pathtmp);
 		pathtmp = strdup(path);
 		for (char *tok = strtok(pathtmp, hardcoded_platform_specific_path_separator); tok; tok = strtok(NULL, hardcoded_platform_specific_path_separator)) {
-			sprintf(ph, "%s/%s.proj%s", tok, file, extention);
-			printf("tryingB %s....   \n", ph);
+			sprintf(ph, "/var/%s/%s.proj%s", tok+4, file, extention);
+			printf("trying %s\n", ph);
 			if (access(ph, F_OK) == 0) {
-	      		printf("foundB at %s\n", ph);
+	      		printf("found '%s' at '%s'\n", file, ph);
 	      		return ph;
 	      	}
 		}
 		free(pathtmp);
 		sprintf(ph, "./%s%s", file, extention);
-		printf("tryingA %s....   \n", ph);
+		printf("trying %s\n", ph);
 		if (access(ph, F_OK) == 0) {
-	      	printf("foundA at %s\n", ph);
+	      	printf("found '%s' at '%s'\n", file, ph);
 	      		return ph;
 	    }
 		sprintf(ph, "./%s.proj%s", file, extention);
-		printf("tryingA %s....   \n", ph);
+		printf("trying %s\n", ph);
 	      if (access(ph, F_OK) == 0) {
-	      	printf("foundA at %s\n", ph);
+	      	printf("found '%s' at '%s'\n", file, ph);
 	      		return ph;
 	      }
 	}
@@ -719,6 +949,7 @@ CPPuint programID = 0;
 
 int builtin__gcc(const char **args) {
 	const char * file = builtin__whereis(args, ".c", true);
+	ps(file)
 	if (strcmp(file,"NULL") == 0) {
 		printf("%s: error: input is NULL\n", args[0]);
 		return -1;
