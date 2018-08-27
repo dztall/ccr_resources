7c98c49dbece4180bcf28dd0cd13b67d38567e84
Lee Jeong Seop <dztall@hanmail.net>
2018-08-27 21:21:00 +0900

    file add

---
+++ Examples/fetch_mobilec_bugs.c
@@ -0,0 +1,11 @@
+﻿//Authors : Lee Jeong Seop
+
+//This program clones Mobile C bugs github repository to Mobile C local folder.
+
+#include <stdlib.h>
+
+int main()
+{
+	system("git2 clone https://github.com/dztall/ccr_bugs mobilec_bugs");
+	return 0;
+}
